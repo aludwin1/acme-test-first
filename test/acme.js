589d47e276584e3df283b028a34f13e5860f1193
Aaron Ludwin <aludwin1@binghamton.edu>
2016-07-11 06:36:51 +0530

    Completed Work

---
+++ test/acme.js
@@ -0,0 +1,58 @@
+var AcmeDb = function() {
+	this.employees = [];
+};
+
+var Employee = function(name, id) {
+	this.name = name;
+	this.id = id;
+};
+
+AcmeDb.prototype.getEmployees = function(){
+	this.employees.sort(function(a, b) {
+		var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+		var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+		if (nameA < nameB) {
+			return -1;
+		} else if (nameA > nameB) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
+	
+	return this.employees;
+};
+
+AcmeDb.prototype.addEmployee = function(name, id) {
+	var newEmployee = new Employee(name, id);
+	this.employees.push(newEmployee);
+};
+
+AcmeDb.prototype.getEmployee = function(id) {
+	for(i = 0; i < this.employees.length; i++) {
+		if(this.employees[i].id === id) {
+			return this.employees[i].name;
+		}
+	}
+};
+
+AcmeDb.prototype.deleteEmployee = function(name){
+	for(i = 0; i < this.employees.length; i++) {
+		if(this.employees[i].name === name) {
+			this.employees.splice(i, 1);
+		}
+	}
+	return this.employees;
+};
+
+AcmeDb.prototype.groupedEmployees = function() {
+	for(i = 0; i < this.employees.length; i++) {
+		if(!this.hasOwnProperty(this.employees[i].name[0])) {
+			this[this.employees[i].name[0]] = [];
+			this[this.employees[i].name[0]].push(this.employees[i]);
+		} else {
+			this[this.employees[i].name[0]].push(this.employees[i]);
+		}
+	}
+	return this;
+};
